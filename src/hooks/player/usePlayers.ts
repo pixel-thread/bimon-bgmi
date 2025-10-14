4319a578f9fde48bc5b877e10a2cd19cac142238
JyrwaJR <jyrwaboys@gmail.com>
2025-10-14 14:24:45 +0530

    type

--- src/hooks/player/usePlayers.ts
+++ src/hooks/player/usePlayers.ts
@@ -1,11 +1,14 @@
 import { Prisma } from "@/src/lib/db/prisma/generated/prisma";
 import http from "@/src/utils/http";
 import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "../useAuth";
+import { PlayerT } from "@/src/types/player";
 
-type PlayerT = Prisma.PlayerGetPayload<{ include: { user: true } }>;
 export function usePlayers() {
+  const { user } = useAuth();
+  const url = user?.role === "SUPER_ADMIN" ? `/admin/players` : `/players`;
   return useQuery({
-    queryFn: () => http.get<PlayerT[]>("/player"),
+    queryFn: () => http.get<PlayerT[]>(url),
     queryKey: ["player"],
     select: (data) => data.data,
   });
