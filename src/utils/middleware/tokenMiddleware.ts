64c95a725e10b8d4765ad051859f028520a5c831
JyrwaJR <jyrwaboys@gmail.com>
2025-11-05 11:27:48 +0530

    tempo to be able to create user

--- src/utils/middleware/tokenMiddleware.ts
+++ src/utils/middleware/tokenMiddleware.ts
@@ -10,7 +10,7 @@ export async function tokenMiddleware(req: NextRequest | Request) {
   const token = authHeader?.split(" ")[1];
 
   if (!token) {
-    throw new UnauthorizedError("Unauthorized 1");
+    throw new UnauthorizedError("Unauthorized");
   }
   // Parse the Clerk session JWT and get claims
   const claims = await verifyToken(token, {
@@ -18,26 +18,26 @@ export async function tokenMiddleware(req: NextRequest | Request) {
   });
 
   if (!claims.sub) {
-    throw new UnauthorizedError("Unauthorized 2");
+    throw new UnauthorizedError("Unauthorized");
   }
 
   // Try to find user in your backend
   const user = await getUserByClerkId({ id: claims.sub });
 
   if (!user) {
-    if (process.env.NODE_ENV === "development") {
-      const clerkUser = await clientClerk.users.getUser(claims.sub);
-      const user = await createUserIfNotExistInDB({
-        clerkId: claims.sub,
-        username: clerkUser?.username || Math.random().toString(36).slice(2),
-      });
-      return user;
-    }
+    // if (process.env.NODE_ENV === "development") {
+    const clerkUser = await clientClerk.users.getUser(claims.sub);
+    const user = await createUserIfNotExistInDB({
+      clerkId: claims.sub,
+      username: clerkUser?.username || Math.random().toString(36).slice(2),
+    });
+    return user;
+    // }
     // If the user is not found, revoke this session at Clerk
-    if (claims.sid) {
-      await clientClerk.sessions.revokeSession(claims.sid);
-    }
-    throw new UnauthorizedError("Unauthorized");
+    // if (claims.sid) {
+    //   await clientClerk.sessions.revokeSession(claims.sid);
+    // }
+    // throw new UnauthorizedError("Unauthorized");
   }
 
   return user;
