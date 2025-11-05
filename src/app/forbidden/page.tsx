9e5e5f13825c63adcbf703310238fcadff964f19
JyrwaJR <jyrwaboys@gmail.com>
2025-11-05 15:03:13 +0530

    update page text

--- src/app/forbidden/page.tsx
+++ src/app/forbidden/page.tsx
@@ -1,25 +1,32 @@
+import { buttonVariants } from "@/src/components/ui/button";
+import { cn } from "@/src/lib/utils";
+import Link from "next/link";
+
 export default function page() {
   return (
     <>
-      <main className="grid min-h-screen flex justify-center  items-center px-6 py-24 sm:py-32 lg:px-8">
+      <main className="min-h-screen flex justify-center  items-center px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-400">401</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-            Page not found
+          <p className="text-lg font-black text-indigo-400">401</p>
+          <h1 className="mt-4 text-5xl font-black tracking-tight text-balance text-red-500 sm:text-7xl">
+            Unauthorized
           </h1>
           <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
+            You are not authorized to view this page. Please contact support.
           </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          <div className="mt-10 flex items-center justify-center gap-x-2">
+            <Link
+              href="/"
+              className={cn(buttonVariants({ variant: "default" }))}
             >
               Go back home
-            </a>
-            <a href="#" className="text-sm font-semibold text-white">
+            </Link>
+            <Link
+              href="/contact"
+              className={cn(buttonVariants({ variant: "outline" }))}
+            >
               Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </div>
       </main>
