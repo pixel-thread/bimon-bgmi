7eb2e99374d7e7c1217424904cdf3d4bf8323fa8
JyrwaJR <jyrwaboys@gmail.com>
2025-11-16 13:18:09 +0530

    return kd and correct category

--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -6,9 +6,34 @@ import { SuccessResponse } from "@/src/utils/next-response";
 import { getMeta } from "@/src/utils/pagination/getMeta";
 import { NextRequest } from "next/server";
 
+function getKdRank(kills: number, deaths: number): string {
+  if (deaths === 0) {
+    // Prevent division by zero; if kills > 0 and no deaths, consider as legend
+    return "bot";
+  }
+
+  const kdRatio = kills / deaths;
+
+  if (kdRatio >= 2.5) {
+    return "legend";
+  } else if (kdRatio >= 2.0) {
+    return "ultra pro";
+  } else if (kdRatio >= 1.5) {
+    return "pro";
+  } else if (kdRatio >= 1.0) {
+    return "noob";
+  } else if (kdRatio >= 0.5) {
+    return "ultra noob";
+  } else if (kdRatio < 0.2) {
+    return "bot";
+  } else {
+    return "bot";
+  }
+}
+
 export async function GET(req: NextRequest) {
   try {
-    const user = await tokenMiddleware(req);
+    await tokenMiddleware(req);
     const query = req.nextUrl.searchParams;
     const page = query.get("page") || "1";
     let seasonId = query.get("season");
@@ -26,15 +51,22 @@ export async function GET(req: NextRequest) {
       page,
       where,
     });
+
     const data = players.map((player) => {
+      const playerKd =
+        player.playerStats.reduce((acc, curr) => acc + curr.kills, 0) /
+          player.playerStats.reduce((acc, curr) => acc + curr.deaths, 0) || 0;
+
       return {
         id: player.id,
         isBanned: player.isBanned,
-        category: player.category,
         userName: player?.user?.userName,
-        matches:
-          player?.playerStats?.find((val) => val.seasonId === seasonId)?.matches
-            .length ?? 0,
+        matches: player?.matchPlayerPlayed.length,
+        kd: playerKd.toFixed(2) || 0,
+        category: getKdRank(
+          player.playerStats.reduce((acc, curr) => acc + curr.kills, 0),
+          player.playerStats.reduce((acc, curr) => acc + curr.deaths, 0),
+        ),
       };
     });
     return SuccessResponse({
