af46abd280bceacfc0e1835a1a893422fe94f00c
JyrwaJR <jyrwaboys@gmail.com>
2025-12-04 23:03:43 +0530

    update

--- src/app/api/admin/temp/route.ts
+++ src/app/api/admin/temp/route.ts
@@ -4,23 +4,40 @@ import { handleApiErrors } from "@/src/utils/errors/handleApiErrors";
 
 export async function POST(req: Request) {
   try {
-    const clerkUsers = await clientClerk.users.getUserList();
+    const limit = 100; // Clerk max limit per page
+    let offset = 0;
+    let hasMore = true;
 
-    for (const clerkUser of clerkUsers.data) {
-      const userName =
-        clerkUser.username ||
-        clerkUser.primaryEmailAddress?.emailAddress.split("@")[0] ||
-        Math.floor(Math.random() * 1000000000).toString();
-      await createUserIfNotExistInDB({
-        data: {
-          password: "123Abc_@",
-          email: clerkUser.primaryEmailAddress?.emailAddress || "",
-          userName: userName,
-          createdBy: "SEED",
-          clerkId: clerkUser.id,
-        },
+    do {
+      // Fetch a page of users
+      const clerkUsers = await clientClerk.users.getUserList({
+        limit,
+        offset,
       });
-    }
+
+      // Loop through users in this page
+      for (const clerkUser of clerkUsers.data) {
+        const userName =
+          clerkUser.username ||
+          clerkUser.primaryEmailAddress?.emailAddress?.split("@")[0] ||
+          Math.floor(Math.random() * 1000000000).toString();
+
+        await createUserIfNotExistInDB({
+          data: {
+            password: "123Abc_@",
+            email: clerkUser.primaryEmailAddress?.emailAddress || "",
+            userName,
+            createdBy: "SEED",
+            clerkId: clerkUser.id,
+          },
+        });
+      }
+
+      // Update pagination
+      offset += limit;
+      hasMore = clerkUsers.data.length === limit;
+    } while (hasMore);
+
     return new Response(JSON.stringify({ message: "success" }), {
       status: 200,
     });
