e474c4d43ba126f6bf4f5f48973fbd0a7639ed96
bimon08 <bimonlangnongsiej@gmail.com>
2026-02-27 22:53:41 +0530

    fix: dashboard loading.tsx was still referencing deleted FunLoader

--- src/app/(dashboard)/loading.tsx
+++ src/app/(dashboard)/loading.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FunLoader } from "@/components/common/fun-loader";
+import { Spinner } from "@heroui/react";
 
 /**
  * Loading state for the dashboard route group.
@@ -8,7 +8,7 @@ import { FunLoader } from "@/components/common/fun-loader";
 export default function DashboardLoading() {
     return (
         <div className="flex min-h-[60vh] items-center justify-center">
-            <FunLoader />
+            <Spinner size="lg" />
         </div>
     );
 }
