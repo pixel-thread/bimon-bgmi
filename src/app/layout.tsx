15ec06eb5b7ef20fca7d7cd6e99b689a5561642d
bimon08 <bimonlangnongsiej@gmail.com>
2026-04-01 23:01:46 +0530

    refactor: replace next/script with standard script tag for AdSense integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from "next";
-import Script from "next/script";
 import { Rajdhani } from "next/font/google";
 import { Providers } from "@/components/providers";
 import { RouteTracker } from "@/components/common/route-tracker";
@@ -93,11 +92,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2651043074081875"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
       </head>
       <body className={`${rajdhani.variable} font-sans antialiased`}>
